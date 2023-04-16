9290984809020a08bdcbba3cb6d947adb1b6b17f
Renat Akhtyamov <akre-t@MacBook-Pro-Renat.local>
2023-04-16 15:50:56 +0300

    initial commit

---
+++ tests_dz3/dz3_1.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+class dz3:
+    def test_phrases(self):
+        phrase = input("Set a phrase: ")
+
+        assert len(phrase) < 15, "Длина строки должна быть короче 15 символов}"
+
+
